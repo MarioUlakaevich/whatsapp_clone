234826745ffff234b09b7708c960f5750597c50a
Marat Ulakaev <ulakaev05@gmail.com>
2022-09-20 17:19:44 +0200

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAe0ZegSKCc4-I6oE-gssjkaFqo5UoP3LI",
+    authDomain: "whatsapp-mern-ef657.firebaseapp.com",
+    projectId: "whatsapp-mern-ef657",
+    storageBucket: "whatsapp-mern-ef657.appspot.com",
+    messagingSenderId: "577453703433",
+    appId: "1:577453703433:web:787222416821290bb713d8"
+  };
